125a68d61470f163fb9c53a5edba9b868f8dad42
laibamaqsood <55451875+laibamaqsood@users.noreply.github.com>
2025-10-20 17:19:08 +0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,42 @@
+# import pandas
+# import subprocess
+
+# sra_accession = "SRR13477610"
+# subprocess.run(["fastq-dump", sra_accession])
+
+
+
+
+# Replace 'your_file.csv' with the actual path to your CSV file
+
+
+import pandas as pd
+import os
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv('gastresult_without_nan.csv')
+
+# Extract rows with adjusted p-values less than 0.05
+positive_logFC_df = df[df['adj.P.Val'] < 0.05]
+
+# Extract specific columns for the positive logFC rows
+selected_columns_df = positive_logFC_df[['ID', 'adj.P.Val', 'P.Value', 't', 'B', 'logFC', 'Gene.symbol', 'Gene.title']]
+
+# Display the DataFrame with selected columns
+print(selected_columns_df)
+
+# Check if the DataFrame is empty
+if selected_columns_df.empty:
+    print("The DataFrame is empty. No data to save.")
+else:
+    try:
+        # Save the selected columns DataFrame to a CSV file
+        selected_columns_df.to_csv("file.csv", index=False)
+        print("File saved successfully.")
+    except Exception as e:
+        print(f"An error occurred while saving the file: {e}")
+
+# Confirm the working directory
+print("Current working directory:", os.getcwd())
+
+
